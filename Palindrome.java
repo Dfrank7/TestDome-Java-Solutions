4184b9a58d60411702bab722834c049bf896d61c
@francisoladipo5 <francisoladipo5@gmail.com>
2020-02-06 18:13:48 +0100

    Replaced palindrome with a shorter solution

--- Palindrome.java
+++ Palindrome.java
@@ -2,36 +2,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class Palindrome {
-    public static boolean isPalindrome(String word) {
-        List<Object> newWord = new ArrayList();
-		char[] wordToChar = word.toCharArray();
-    	for(int i = wordToChar.length-1; i>=0; i--) {
-    		newWord.add(wordToChar[i]);
-    	}
-    	String arrays = newWord.toString();
-    	String words = listToString(newWord);
-    	if(word.toLowerCase().equals(words.toLowerCase())) {
-        	return true;
-        	}else {
-        		return false;
-    }
+    private static Boolean Palindrome(String word){
+        String[] wa = word.split("");
+        int count =0;
+        int len = word.length();
+
+        for (int i = 0; i<word.length(); i++){
+            if (wa[i].toLowerCase().equals(wa[len-i-1].toLowerCase())){
+                count++;
+                //count++;
+            }else {
+                count = 0;
+                break;
+            }
+        }
+        if (count>0){
+            return true;
+        }else {
+            return false;
+        }
     }
     
     public static void main(String[] args) {
         System.out.println(Palindrome.isPalindrome("Deleveled"));
-    }
-    public static String listToString(List<Object> list) {
-        StringBuilder sb = new StringBuilder();
-
-        for (Object object : list) {
-            if (object != null) {
-                if (object.getClass().isArray()) {
-                    sb.append(Arrays.toString((Object[]) object));
-                } else {
-                    sb.append(object.toString());
-                }
-            }
-        }
-        return sb.toString();
     }
 }
